ec2575f30c4d41e820ec8bffe18a710f9c5cd58d
Santiago <santifl.17@gmail.com>
2020-03-28 12:40:44 -0500

    Api Final v1
    Api Productos

--- Mottiv/api/views.py
+++ Mottiv/api/views.py
@@ -1,12 +1,24 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
-from .serializers import MovieSerializer
+from .serializers import MovieSerializer, MovieMiniSerializer
 from .models import Movie
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
+class MySampleView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        return Response(data={"status":True})
 
 class MovieViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+
+    def list(self, request, *args, **kwargs):
+        movies = Movie.objects.all()
+        serializer = MovieMiniSerializer(movies, many=True)
+        return Response(serializer.data)
+
+
--- Mottiv/urls.py
+++ Mottiv/urls.py
@@ -26,5 +26,6 @@ router.register(r'movies', views.MovieViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+
 ]
